a30f658b3842eef7034ba0ec3cf24bdc8e0fdda8
MatheoM35 <184085566+MatheoM35@users.noreply.github.com>
2025-04-22 00:33:18 +0200

    Implement registration handling with form submission and service integration

--- src/Controller/RegisterController.php
+++ src/Controller/RegisterController.php
@@ -3,16 +3,31 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
-use App\Form\RegisterType;
+use App\Form\Type\RegisterType;
+use App\Service\RegisterService;
 
 class RegisterController extends AbstractController
 {
+    public function __construct(
+        private readonly RegisterService $registerService
+    ) {
+    }
+
     #[Route('/register', name: 'app_register')]
-    public function index(): Response
+    public function index(Request $request): Response
     {
-        $form = $this->createForm(RegisterType::class);
+        $form = $this
+            ->createForm(RegisterType::class)
+            ->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $this->registerService -> register($form->getData());
+        }
+
         return $this->render('register/index.html.twig', [
             'form' => $form->createView(),
         ]);
---
+++ src/Service/RegisterService.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Service;
+
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class RegisterService
+{
+    public function __construct(
+        private readonly EntityManagerInterface $em,
+        private readonly UserPasswordHasherInterface $passwordHasher
+    )
+    {
+    }
+
+    public function register(User $user): void
+    {
+        // Hash the password
+        $hashedPassword = $this->passwordHasher->hashPassword($user, $user->getPassword());
+        $user->setPassword($hashedPassword);
+
+        dd($user);
+
+        // Persist the user entity
+        $this->em->persist($user);
+        $this->em->flush();
+    }
+}
