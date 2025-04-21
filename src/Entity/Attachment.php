5ca2d856d6d73bd265d8524d140bcf65d249ece2
MatheoM35 <184085566+MatheoM35@users.noreply.github.com>
2025-04-21 22:48:54 +0200

    Add Attachment entity and AttachmentRepository for file management

---
+++ src/Entity/Attachment.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\AttachmentRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: AttachmentRepository::class)]
+class Attachment
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $originalName = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $path = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getOriginalName(): ?string
+    {
+        return $this->originalName;
+    }
+
+    public function setOriginalName(string $originalName): static
+    {
+        $this->originalName = $originalName;
+
+        return $this;
+    }
+
+    public function getPath(): ?string
+    {
+        return $this->path;
+    }
+
+    public function setPath(string $path): static
+    {
+        $this->path = $path;
+
+        return $this;
+    }
+}
